11835c0860ed12a2ac3e96b79c41cb6afca14efa
Goos <goos.bekerom@gmail.com>
2018-03-29 22:15:31 +0200

    Fixed a problem where apparently in roulette the color order of red and black makes no sense

--- src/main/kotlin/com/goosvandenbekerom/roulette/core/RouletteColor.kt
+++ src/main/kotlin/com/goosvandenbekerom/roulette/core/RouletteColor.kt
@@ -1,13 +1,13 @@
 package com.goosvandenbekerom.roulette.core
 
 enum class RouletteColor {
-    RED, BLACK, GREEN, ANY;
+    RED, BLACK, GREEN;
 
     companion object {
         fun from(number: Int) : RouletteColor {
             if (number == ZERO || number == DOUBLE_ZERO) return GREEN
-            return if (number % 2 == 0) BLACK
-            else RED
+            return if (number in intArrayOf(1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36)) RED
+            else BLACK
         }
     }
 }
