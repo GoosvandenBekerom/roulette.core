7d05b8b0c2fc60084516049de6b048c20b3702dd
Goos <goos.bekerom@gmail.com>
2018-03-29 21:15:56 +0200

    Made it easier to initialize a result

--- src/main/kotlin/com/goosvandenbekerom/roulette/core/Result.kt
+++ src/main/kotlin/com/goosvandenbekerom/roulette/core/Result.kt
@@ -2,6 +2,6 @@ package com.goosvandenbekerom.roulette.core
 
 import java.util.Date
 
-data class Result(val color: RouletteColor, val number: Int, val timestamp: Date){
+data class Result(val number: Int, val color: RouletteColor = RouletteColor.from(number), val timestamp: Date = Date()){
     override fun toString() = "Result(color=$color, number=$number)"
 }
--- src/main/kotlin/com/goosvandenbekerom/roulette/core/Wheel.kt
+++ src/main/kotlin/com/goosvandenbekerom/roulette/core/Wheel.kt
@@ -6,8 +6,6 @@ class Wheel(gameId: Long) {
     private val rng = Random(gameId)
 
     fun spin() : Result {
-        val number = rng.nextInt(RANDOM_UPPER_BOUND)
-        val color = RouletteColor.from(number)
-        return Result(color, number, Date())
+        return Result(rng.nextInt(RANDOM_UPPER_BOUND))
     }
 }
